ba350e741ea9bcaf01b1bed1ca8d18a7e386ed47
LewisMac23 <124689702+LewisMac23@users.noreply.github.com>
2023-02-27 13:28:16 +0000

    Add files via upload

---
+++ HairSalonProjectLocalDraft2.cpp
@@ -0,0 +1,19 @@
+// HairSalonProjectLocalDraft2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "appointment.h"
+using namespace std;
+
+int main()
+{
+    cout << "Please enter your first name" << endl;
+    getline.firstname;
+    cout << "Please enter your surname" << endl;
+    getline.surname;
+
+    return 0;// into the other mob #watp #ftp #dreams never die fs10 
+}
+
---
+++ appointment.cpp
@@ -0,0 +1,18 @@
+#include "appointment.h"
+
+string appointment::getName(string firstname, string surname) {
+	name = firstname + surname;
+	return name;
+};
+void appointment::getDate() {
+
+};
+void appointment::checkTimes() {
+
+};
+void appointment::addAppointment() {
+
+};
+void appointment::cancelAppointment() {
+
+};
