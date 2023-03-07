eb9d92b531cbb19278044e0ca9bf4611d4c23c56
LewisMac23 <124689702+LewisMac23@users.noreply.github.com>
2023-03-07 12:50:36 +0000

    Add files via upload

--- HairSalonProjectLocalDraft2.cpp
+++ HairSalonProjectLocalDraft2.cpp
@@ -2,18 +2,41 @@
 //
 
 #include <iostream>
+#include <ctime>
+#include <vector>
 #include <string>
 #include <fstream>
 #include "appointment.h"
 using namespace std;
 
+struct appointment {
+    string firstname;
+    string surname;
+    tm date_time;
+};
+
 int main()
 {
+    vector <appointment> appointments;
+
     cout << "Please enter your first name" << endl;
-    getline.firstname;
+    string firstname;
+    getline(cin, firstname);
+
     cout << "Please enter your surname" << endl;
-    getline.surname;
+    string surname;
+    getline(cin, surname);
+
+    cout << "Please enter a date for your booking(DD/MM/YYYY  HH:MM)";
+    string appointment_date_time_str;
+    getline(cin, appointment_date_time_str);
+
+    tm appointment_date_time = {};
+
+
+    appointment Appointment = { firstname, surname, appointment_date_time };
+    appointments.push_back(Appointment);
 
     return 0;// into the other mob #watp #ftp #dreams never die fs10 
-}
+};
 
